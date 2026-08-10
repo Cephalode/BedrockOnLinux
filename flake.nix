daedca66c967b833fd6b6a2c5b995dee45ba7dc1
Cephalode <invoice@cephalode.com>
2026-08-10 18:49:39 -0400

    nix: attribute homepage to upstream

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
         '';
 
         meta = {
-          homepage = "https://github.com/Cephalode/BedrockOnLinux";
+          homepage = "https://github.com/Wyze3306/BedrockOnLinux";
           license = pkgs.lib.licenses.mit;
           platforms = [ "x86_64-linux" ];
           mainProgram = "bedrock-on-linux";
